7333047fc76852844986aa7a21581c9e3fbd58c9
vadim.vainshtein <vadim.vainshtein@devexperts.com>
2024-07-26 01:26:35 +0500

    initial commit

---
+++ src/main/java/kz/vainshtein/kayakbotcore/KayakBotCoreApplication.java
@@ -0,0 +1,13 @@
+package kz.vainshtein.kayakbotcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayakBotCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KayakBotCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/vainshtein/kayakbotcore/KayakBotCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.vainshtein.kayakbotcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayakBotCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
