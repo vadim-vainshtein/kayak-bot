fb683355d44cabd91303b2209919763a26a26033
Slava <slavon1998x359x@gmail.com>
2024-08-16 18:13:23 +0500

    changed directory of class Training

---
+++ src/main/java/kz/kayaker/botcore/entity/Training.java
@@ -0,0 +1,36 @@
+package kz.vainshtein.kayakbotcore;
+
+import jakarta.persistence.Entity;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+public class Training {
+    private DayOfWeek trainingDay;
+    private LocalTime trainingTime;
+
+    public Training(int dayOfWeek, int hour, int minute) {
+        trainingDay = DayOfWeek.of(dayOfWeek);
+        trainingTime = LocalTime.of(hour, minute);
+    }
+
+    public DayOfWeek getTrainingDay() {
+        return trainingDay;
+    }
+
+    public LocalTime getTrainingTime() {
+        return trainingTime;
+    }
+
+    public void setTrainingDay(int dayOfWeek) {
+        trainingDay = DayOfWeek.of(dayOfWeek);
+    }
+
+    public void setTrainingTime(int hour, int minute) {
+        trainingTime = LocalTime.of(hour, minute);
+    }
+
+}
